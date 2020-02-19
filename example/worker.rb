bb5ecb6f89f41d671a498901a273ce98e3fd50cd
Chris Thorn <chris@thorn.co>
2020-02-19 15:57:30 -0800

    Add procfile for development processes
    This is a convenience when manually testing things locally. It may also
    serve as a reminder of what the current CLI interface is help drive
    improvements.

--- example/worker.rb
+++ example/worker.rb
@@ -6,7 +6,7 @@ class PlaygroundWorker < Velveteen::Worker
   self.queue_name = "velveteen_general_development"
   self.routing_key = "velveteen.general.development"
   # self.message_schema = "velveteen_general.json"
-  # self.rate_limit_key = "velveteen-general-development"
+  self.rate_limit_key = "velveteen-general-development"
 
   def perform
     if message.data[:job].even? && rand > 0.5
