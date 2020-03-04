2c8d6f82b1bff59cfdb625eb6c2123ef68f36ab0
Chris Thorn <chris@thorn.co>
2020-03-04 15:24:57 -0800

    Consistent error handler interface
    In c6d479a8 the reject backoff handler was updated to accept a worker
    class, instead of an instance, because some errors happen before a
    worker is instantiated and resulting in passing in `nil`. That change
    left the two error handlers with a slightly different interface, which
    is corrected here.

--- lib/velveteen/error_handlers/reject.rb
+++ lib/velveteen/error_handlers/reject.rb
@@ -1,7 +1,7 @@
 module Velveteen
   module ErrorHandlers
     class Reject
-      def self.call(error:, message:, worker:)
+      def self.call(error:, message:, worker_class:)
         Config.channel.reject(message.delivery_info.delivery_tag, false)
         Velveteen.logger.error(error.message)
         Velveteen.logger.debug(error.backtrace)
--- spec/velveteen/error_handlers/reject_spec.rb
+++ spec/velveteen/error_handlers/reject_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe Velveteen::ErrorHandlers::Reject do
   it "rejects the message" do
     delivery_info = double(delivery_tag: "foo")
     message = double(delivery_info: delivery_info)
-    worker = double
     error = StandardError.new
     channel = double(reject: true)
     allow(Velveteen::Config).to receive(:channel).and_return(channel)
 
-    described_class.call(error: error, message: message, worker: worker)
+    described_class.call(error: error, message: message, worker_class: double)
 
     expect(Velveteen::Config.channel).to have_received(:reject)
       .with("foo", false)
